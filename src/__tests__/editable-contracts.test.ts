f1ea1f16fb49b1f8c8b1e9a958b9a0dbc631501c
Kirill Anoshin <kanoshin@servicetitan.com>
2019-03-28 22:18:09 -0700

    : MobX integration

--- src/__tests__/editable-contracts.test.ts
+++ src/__tests__/editable-contracts.test.ts
@@ -1,26 +1,28 @@
-import { editable } from '../editable-contracts';
+import { editable, Editable } from '../editable-contracts';
+import { autorun } from 'mobx';
 
-test('editable', () => {
-    enum InventoryType {
-        Furniture,
-        Electronics,
-        Tools,
-        Jewelry
-    }
-    interface InventoryDto {
-        id: number;
-        age: number;
-        name: string;
-        type?: InventoryType | undefined;
-        location?: string | undefined;
-        count?: number | undefined;
-        metadata?: {
-            key0: boolean;
-            key1?: string | undefined;
-            key2?: number | undefined;
-            key3?: number[] | undefined;
-        };
-    }
+enum InventoryType {
+    Furniture,
+    Electronics,
+    Tools,
+    Jewelry
+}
+interface InventoryDto {
+    id: number;
+    age: number;
+    name: string;
+    type?: InventoryType | undefined;
+    location?: string | undefined;
+    count?: number | undefined;
+    metadata?: {
+        key0: boolean;
+        key1?: string | undefined;
+        key2?: number | undefined;
+        key3?: number[] | undefined;
+    };
+}
+
+function editableInventory(): [InventoryDto, Editable<InventoryDto>] {
     const inventoryDto: InventoryDto = {
         id: 1,
         age: 100,
@@ -30,38 +32,124 @@ test('editable', () => {
             key3: [1, 2, 3]
         }
     };
-    const editableInventoryDto = editable(inventoryDto);
+    return [inventoryDto, editable(inventoryDto)];
+}
 
-    expect(editableInventoryDto.value).toEqual(inventoryDto);
-
-    expect(editableInventoryDto.$.id.value).toBe(1);
-    expect(editableInventoryDto.$.location.value).toBe(undefined);
-    editableInventoryDto.$.location.onChange('San Diego');
-    expect(editableInventoryDto.$.location.value).toBe('San Diego');
-    editableInventoryDto.$.metadata.$!.key1.onChange('best choice');
-    expect(editableInventoryDto.$.metadata.$!.key1.value).toBe('best choice');
-
-    expect(editableInventoryDto.$.metadata.$!.key3.value).toEqual([1, 2, 3]);
-    expect(editableInventoryDto.$.metadata.$!.key3.$![0].value).toBe(1);
-    expect(editableInventoryDto.$.metadata.$!.key3.$![2].value).toBe(3);
-    editableInventoryDto.$.metadata.$!.key3.$![2].onChange(4);
-    expect(editableInventoryDto.$.metadata.$!.key3.$![2].value).toBe(4);
-
-    editableInventoryDto.$.type.onChange(InventoryType.Jewelry);
-    expect(editableInventoryDto.$.type.value).toBe(InventoryType.Jewelry);
-
-    const metadata: InventoryDto['metadata'] = {
-        key0: true,
-        key1: undefined,
-        key2: 123,
-        key3: [1]
-    };
-    editableInventoryDto.$.metadata.onChange(metadata);
-    expect(editableInventoryDto.$.metadata.value).toEqual(metadata);
-
-    editableInventoryDto.$.metadata.onChange(undefined);
-    expect(() => editableInventoryDto.$.metadata.$!.key0).toThrow();
+describe('onChange', () => {
+    // TODO: split to multiple tests
+    test('editable', () => {
+        const [inventoryDto, editableInventoryDto] = editableInventory();
+    
+        expect(editableInventoryDto.value).toEqual(inventoryDto);
+    
+        expect(editableInventoryDto.$.id.value).toBe(1);
+        expect(editableInventoryDto.$.location.value).toBe(undefined);
+        editableInventoryDto.$.location.onChange('San Diego');
+        expect(editableInventoryDto.$.location.value).toBe('San Diego');
+        editableInventoryDto.$.metadata.$!.key1.onChange('best choice');
+        expect(editableInventoryDto.$.metadata.$!.key1.value).toBe('best choice');
+    
+        expect(editableInventoryDto.$.metadata.$!.key3.value).toEqual([1, 2, 3]);
+        expect(editableInventoryDto.$.metadata.$!.key3.$![0].value).toBe(1);
+        expect(editableInventoryDto.$.metadata.$!.key3.$![2].value).toBe(3);
+        editableInventoryDto.$.metadata.$!.key3.$![2].onChange(4);
+        expect(editableInventoryDto.$.metadata.$!.key3.$![2].value).toBe(4);
+    
+        editableInventoryDto.$.type.onChange(InventoryType.Jewelry);
+        expect(editableInventoryDto.$.type.value).toBe(InventoryType.Jewelry);
+    
+        const metadata: InventoryDto['metadata'] = {
+            key0: true,
+            key1: undefined,
+            key2: 123,
+            key3: [1]
+        };
+        editableInventoryDto.$.metadata.onChange(metadata);
+        expect(editableInventoryDto.$.metadata.value).toEqual(metadata);
+    
+        editableInventoryDto.$.metadata.onChange(undefined);
+        expect(() => editableInventoryDto.$.metadata.$!.key0).toThrow();
+    });
 });
+
+describe('MobX', () => {
+    test('shallow prop is observable', () => {
+        const [, editableInventoryDto] = editableInventory();
+        const result: string[] = [];
+        autorun(() => {
+            result.push(editableInventoryDto.$.name.value);
+        });
+        editableInventoryDto.$.name.onChange('necklace');
+        expect(result).toEqual(['ring', 'necklace']);
+    });
+
+    test('deep prop is observable', () => {
+        const [, editableInventoryDto] = editableInventory();
+        const result: (string | undefined)[] = [];
+        autorun(() => {
+            result.push(editableInventoryDto.$.metadata.$!.key1.value);
+        });
+        editableInventoryDto.$.metadata.$!.key1.onChange('necklace');
+        expect(result).toEqual([undefined, 'necklace']);
+    });
+
+    test('deep array prop is observable', () => {
+        const [, editableInventoryDto] = editableInventory();
+        const result: number[] = [];
+        autorun(() => {
+            result.push(editableInventoryDto.$.metadata.$!.key3.$![0].value);
+        });
+        editableInventoryDto.$.metadata.$!.key3.$![0].onChange(2);
+        expect(result).toEqual([1, 2]);
+    });
+    
+    test('top level value is observing shallow changes', () => {
+        const [, editableInventoryDto] = editableInventory();
+        const result: string[] = [];
+        autorun(() => {
+            result.push(editableInventoryDto.value.name);
+        });
+        editableInventoryDto.$.name.onChange('necklace');
+        expect(result).toEqual(['ring', 'necklace']);
+    });
+
+    test('top level value is observing deep changes', () => {
+        const [, editableInventoryDto] = editableInventory();
+        const result: number[] = [];
+        autorun(() => {
+            result.push(editableInventoryDto.value.metadata!.key3![0]);
+        });
+        editableInventoryDto.$.metadata.$!.key3.$![0].onChange(2);
+        expect(result).toEqual([1, 2]);
+    });
+
+    test('top level value is observing top level changes', () => {
+        const [inventoryDto, editableInventoryDto] = editableInventory();
+        const result: InventoryDto[] = [];
+        autorun(() => {
+            result.push(editableInventoryDto.value);
+        });
+        const newInventoryDto = {
+            id: 2,
+            age: 10,
+            name: 'table'
+        };
+        editableInventoryDto.onChange(newInventoryDto);
+        expect(result).toEqual([inventoryDto, newInventoryDto]);
+    });
+
+    test('array is observable', () => {
+        const [, editableInventoryDto] = editableInventory();
+        const result: number[] = [];
+        autorun(() => {
+            result.push(editableInventoryDto.value.metadata!.key3![0]);
+        });
+        editableInventoryDto.$.metadata.$!.key3.$![0].onChange(2);
+        expect(result).toEqual([1, 2]);
+    });
+})
+
+
 
 /* Type tests */
 enum E {
--- src/editable-contracts.ts
+++ src/editable-contracts.ts
@@ -1,3 +1,5 @@
+import { observable, entries, set } from 'mobx';
+
 type Node<T> = {
     value: T;
     onChange(value: T): void;
@@ -14,7 +16,7 @@ type CompositeNode<T> = {
 
 type PrimitiveType = string | number | boolean | Date;
 
-type Editable<T> = NonNullable<T> extends PrimitiveType ? Node<T> : CompositeNode<T>;
+export type Editable<T> = NonNullable<T> extends PrimitiveType ? Node<T> : CompositeNode<T>;
 
 function isEditableObject(obj?: any): obj is object & { [key: string]: Node<any> } {
     return !isEditablePrimitive(obj) && !Array.isArray(obj);
@@ -27,10 +29,10 @@ function isEditablePrimitive(obj?: any) {
 function createTargetValue<T>(data: T) {
     let val: any;
     if (isEditableObject(data)) {
-        val = {};
+        val = observable({}, undefined, { deep: false });
         Object.keys(data).forEach(key => val[key] = editable(data[key]));
     } else if (Array.isArray(data)) {
-        val = [];
+        val = observable([], undefined, { deep: false });
         data.forEach(value => val.push(editable(value)));
     } else {
         val = data;
@@ -39,39 +41,50 @@ function createTargetValue<T>(data: T) {
 }
 
 export function editable<T>(data: T): Editable<T> {
-    const target: Node<any> = {
-        value: createTargetValue(data),
-        onChange(value) {
-            target.value = createTargetValue(value);
+    const target = observable({
+        proxyValue: createTargetValue(data),
+        get value(): any {
+            if (isEditablePrimitive(this.proxyValue)) {
+                return this.proxyValue;
+            }
+            if (Array.isArray(this.proxyValue)) {
+                return this.proxyValue.map(item => item.value);
+            }
+            const value: { [key: string]: any } = {};
+            entries(this.proxyValue).forEach(([key, proxy]) => {
+                value[key] = proxy.value
+            });
+            return value;
+        },
+        onChange(value: any) {
+            this.proxyValue = createTargetValue(value);
         }
-    };
+    });
 
     const proxy = new Proxy(target, {
         get(obj, prop) {
+            if (typeof prop === 'symbol') {
+                return (obj as any)[prop];
+            }
             if (prop === '$') {
                 return proxy;
             }
             if (prop === 'value') {
-                if (isEditablePrimitive(obj.value)) {
-                    return obj.value;
-                }
-                if (Array.isArray(obj.value)) {
-                    return obj.value.map(item => item.value);
-                }
-                const value: { [key: string]: any } = {};
-                Object.keys(obj.value).forEach(key => value[key] = obj.value[key].value);
-                return value;
+                return obj.value;
             }
             if (prop === 'onChange') {
                 return obj.onChange;
             }
-            if (obj.value === undefined) {
+            if (obj.proxyValue === undefined) {
                 throw new Error(`Cannot read property '${prop.toString()}' of undefined`);
             }
-            if (!(prop in obj.value)) {
-                obj.value[prop] = editable(undefined);
+            if (isEditablePrimitive(obj.proxyValue)) {
+                throw new Error(`Cannot get property '${prop.toString()}' from primitive value`);
             }
-            return obj.value[prop];
+            if (!(prop in obj.proxyValue)) {
+                set(obj.proxyValue, { [prop]: editable(undefined) });
+            }
+            return obj.proxyValue[prop];
         }
     }) as any as Editable<T>;
     return proxy;
